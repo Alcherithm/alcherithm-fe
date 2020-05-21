6d28ae8c41ae1a9a2c32d1cbb55d6904b4f403aa
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-21 09:16:13 -0700

    updated app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,9 @@
-import React, { useState } from 'react';
-// import User from '../User/User';
-// import Test from '../Test/Test';
-// import Tester from '../QUnit/QUnit.js';
+import React from 'react';
 import Challenge from '../../containers/Challenge.jsx';
 
 export default function App() {
-  const [code, setCode] = useState('function myFn() {}');
   return (
     <>
-      {/* <textarea value={code} onChange={({ target }) => setCode(target.value)}></textarea> */}
-      {/* <User />
-      <br></br>
-      <Test />
-      <Tester tests={`${code} \n \n QUnit.test("hello", (assert) => assert.equal(myFn(), 1))`} /> */}
       <Challenge />
     </>
   );
